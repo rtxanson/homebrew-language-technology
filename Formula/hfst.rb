1f06a536aec6a7457c6df3f084f2462b1760c357
Ryan <ryan.txanson@gmail.com>
2015-01-29 14:44:04 -0600

    hfst options, update url

--- Formula/hfst.rb
+++ Formula/hfst.rb
@@ -4,17 +4,22 @@ require 'formula'
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Hfst < Formula
-  homepage ''
-  url 'http://downloads.sourceforge.net/project/hfst/hfst/hfst-3.4.5.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fhfst%2Ffiles%2Fhfst%2F&ts=1372822903&use_mirror=superb-dca2'
-  sha1 'fc1fbabd91343f0da3255624f0de2a7ad8f81b49'
+  homepage 'http://hfst.sourceforge.net/'
+  head "svn://svn.code.sf.net/p/hfst/code/trunk hfst-code"
+  url 'http://downloads.sourceforge.net/project/hfst/hfst/archive/hfst-3.8.1.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fhfst%2Ffiles%2F&ts=1422564128&use_mirror=softlayer-dal'
+  sha1 '56592bfab53a055e07e216be0ae5822cdae2db7e'
 
   # depends_on 'cmake' => :build
   # depends_on :x11 # if your formula requires any X11/XQuartz components
+  #
+  # TODO: --with-unicode-handler ?
+
+  option "with-xfst", "Build XFST tool"
 
   def install
     # ENV.j1  # if your formula's build system can't parallelize
 
-    system "./configure"
+    system "./configure", *opts
     system "make"
     # system "cmake", ".", *std_cmake_args
     system "make", "install" # if this fails, try separate make/make install steps
